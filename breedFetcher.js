da8b814831785d0050eea42cbc566b4695c786c5
Chris Hatcher <hatcherchris20@gmail.com>
2022-01-22 19:34:42 +0000

    adding files to json the cat

---
+++ breedFetcher.js
@@ -0,0 +1,1 @@
+const request = require('request');    // Install and use the request library to make the HTTP request (We know this library is deprecated but it is still ok to use for our purposes.)
